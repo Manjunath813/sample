0c00952f8218b67cf38561d1a937aa6b2676b109
Manjunath813 <manju333k@gmail.com>
2022-05-03 21:24:27 +0530

    added project

---
+++ hello/src/main/java/tm/hello/hello1.java
@@ -0,0 +1,17 @@
+package tm.hello;
+
+public class hello1 {
+	
+	public static void main(String[] args)
+	
+	
+	
+	
+	{int a; a=10;
+		System.out.println("hello");}
+	{int b=20; int c =10; 
+		System.out.println("hello1 ad" +c);}
+	
+	
+
+}
